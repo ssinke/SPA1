c149313547d7495b0bcfcf6e7691258c3d7ae0dd
Sinisa Sladic <ssinke95@gmail.com>
2015-05-10 19:01:11 +0200

    Prekopirao sam metode, jer sam bio glup i nisam isao na vezbe pa nisam znao da se to ne treba uciti vec samo primenjivati metode >GLUPOST lv 1000<

--- src/kolokvijum3/Skup.java
+++ src/kolokvijum3/Skup.java
@@ -1,5 +1,5 @@
 package kolokvijum3;
-
+import java.lang.reflect.Array;
 public class Skup<T> {
 	class Element{
 		T info;
@@ -17,10 +17,12 @@ public class Skup<T> {
 	}
 	
 	Element zaglavlje;
+	int brojac;
 	
 	public Skup(){
 		zaglavlje = new Element();
 		zaglavlje.veza = zaglavlje;
+		brojac=0;
 	}
 	
 	public boolean pripada(T clan){
@@ -37,6 +39,7 @@ public class Skup<T> {
 			Element novi = new Element(clan);
 			novi.veza=zaglavlje.veza;
 			zaglavlje.veza=novi;
+			brojac++;
 			return true;
 		}else{
 			return false;
@@ -57,11 +60,107 @@ public class Skup<T> {
 	      return rezultat + " }";
 	}
 	
-	public void stampajSve(){
+	public int velicina() {
+		return brojac;
+	}
+	
+	public boolean jePrazan() {
+		return brojac == 0;
+	}
+	
+	public boolean izbaci(T clan) {
+		if (clan == null)
+			return false;
+		zaglavlje.info = clan;
+		Element prethodni = zaglavlje;
+		while (!prethodni.veza.info.equals(clan)) {
+			prethodni = prethodni.veza;
+		}
+		if (prethodni.veza != zaglavlje) {
+			prethodni.veza = prethodni.veza.veza;
+			brojac--;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void ubaciSve(Skup<T> skup) {
+		if (skup == null)
+			return;
+
+		Element tekuci = skup.zaglavlje.veza;
+		while (tekuci != skup.zaglavlje) {
+			ubaci(tekuci.info);
+			tekuci = tekuci.veza;
+		}
+	}
+	
+	public Skup<T> unija(Skup<T> drugi) {
+		Skup<T> rezultat = new Skup<T>();
+		rezultat.ubaciSve(this);
+		rezultat.ubaciSve(drugi);
+		return rezultat;
+	}
+	
+	public Skup<T> presek(Skup<T> drugi) {
+		Skup<T> rezultat = new Skup<T>();
+		if (drugi != null) {
+			Element tekuci = zaglavlje.veza;
+			while (tekuci != zaglavlje) {
+				if (drugi.pripada(tekuci.info)) {
+					rezultat.ubaci(tekuci.info);
+				}
+				tekuci = tekuci.veza;
+			}
+		}
+		return rezultat;
+	}
+	
+	public Skup<T> razlika(Skup<T> drugi) {
+		Skup<T> rezultat = new Skup<T>();
+		if (drugi != null) {
+			Element tekuci = zaglavlje.veza;
+
+			while (tekuci != zaglavlje) {
+				if (!drugi.pripada(tekuci.info)) {
+					rezultat.ubaci(tekuci.info);
+				}
+				tekuci = tekuci.veza;
+			}
+		} else
+			rezultat.ubaciSve(this);
+		return rezultat;
+	}
+	
+	public boolean podskupOd(Skup<T> drugi) {
+		if (drugi == null)
+			return brojac == 0;
 		Element tekuci = zaglavlje.veza;
-		while(tekuci!=zaglavlje){
-			System.out.println(tekuci.info);
-			tekuci=tekuci.veza;
+		while (tekuci != zaglavlje) {
+			if (!drugi.pripada(tekuci.info)) {
+				return false;
+			}
+			tekuci = tekuci.veza;
+		}
+		return true;
+	}
+	
+	public T[] napraviNiz() {
+		if (brojac > 0) {
+			@SuppressWarnings("unchecked")
+			T[] rez = (T[]) Array
+					.newInstance(zaglavlje.info.getClass(), brojac);
+
+			int i = 0;
+			Element tekuci = zaglavlje.veza;
+			while (tekuci != zaglavlje) {
+				rez[i++] = tekuci.info;
+				tekuci = tekuci.veza;
+			}
+			return rez;
+		} else {
+			return null;
 		}
 	}
 }
