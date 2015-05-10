793d5b8920d8ecb3fc017c49574e3b92cc5e5cee
Sinisa Sladic <ssinke95@gmail.com>
2015-05-10 19:10:44 +0200

    Uradjen prvi zadatak, lagano.

---
+++ src/kolokvijum3/prakticne1.java
@@ -0,0 +1,21 @@
+package kolokvijum3;
+
+import org.svetovid.Svetovid;
+
+public class prakticne1 {
+	public static void prviZadatak(){
+		Skup<String> s = new Skup<String>();
+		String u = "a";
+		do{
+			u = Svetovid.in.readString("Unesite string:");
+			if(!u.equals("."))
+				s.ubaci(u);
+		}while(!u.equals("."));
+		
+		System.out.println(s);
+	}
+	public static void main(String[] args) {
+		prviZadatak();
+	}
+
+}
